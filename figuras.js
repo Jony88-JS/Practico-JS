ec4ab28f0c307f8ea1a80345095ce208aac057a2
Jony88-JS <axe_gomez@hotmail.com>
2021-12-10 17:49:19 -0600

    Agregando form html codigo JS para operaciones fig. geometricas

--- figuras.js
+++ figuras.js
@@ -80,3 +80,57 @@ console.log("El perimetro del circulo es: " + perimetroCirculo + "cm ");
 const areaCirculo = (radioCirculo * radioCirculo) * PI;
 console.log("El area del circulo es: " + areaCirculo + "cm^2 ");*/
 console.groupEnd();
+
+/*Aqui interactuamos con HTML*/
+/*CUADRADO OPERACION JS*/
+function calcularPerimetroCuadrado(){
+    const input = document.getElementById("InputCuadrado");
+    const value = input.value;
+
+    const perimetro = perimetroCuadrado(value);
+    alert("El perimetro del cuadrado es: " + perimetro);
+}
+
+function calcularAreaCuadrado(){
+    const input = document.getElementById("InputCuadrado");
+    const value = input.value;
+
+    const area = areaCuadrado(value);
+    alert("El area del cuadrado es: " + area);
+}
+
+/*TRIANGULO OPERACION JS*/
+function calcularPerimetroTriangulo(){    
+    const lado1 = parseInt(document.getElementById("InputTrianguloL1").value);
+    const lado2 = parseInt(document.getElementById("InputTrianguloL2").value);
+    const base = parseInt(document.getElementById("InputTrianguloBa").value);
+    const perimetro = perimetroTriangunlo(lado1, lado2, base);
+    alert("El perimetro del triangulo es: " + perimetro);  
+}
+
+function calcularAreaTriangulo(){    
+    const input2 = document.getElementById("InputTrianguloAlt");
+    const value2 = parseInt(input2.value);
+    const input3 = document.getElementById("InputTrianguloBa");
+    const value3 = parseInt(input3.value);
+    const area = areaTriangulo(value2,value3);
+    alert("El area del triangulo es: " + area);
+ }
+
+ /*CIRCULO OPERACION JS*/
+ function calcularPerimetroCirculo(){
+    const input = document.getElementById("InputCirculo");
+    const value = input.value;
+
+    const perimetro = perimetroCirculo(value);
+    alert("El perimetro del circulo es: " + perimetro);
+}
+
+function calcularAreaCirculo(){
+    const Area = document.getElementById("InputCirculo");
+    const A = parseFloat(Area.value);
+
+    const area = Math.round(areaCirculo(A));
+    alert("El Area de tu circulo es de: " + area + " cm");
+}
+
